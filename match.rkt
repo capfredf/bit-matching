b92b98ca510ffff10ea9f4245c7d85171591e9da
Joshua Larkin <joslarki@iu.edu>
2018-11-21 16:21:53 -0600

    more

--- match.rkt
+++ match.rkt
@@ -2,17 +2,24 @@
 
 (require (for-syntax syntax/parse))
 
-(define z 10)
-
-
 (begin-for-syntax
   (define-syntax-class pat
     #:attributes ((vars 1) (test 0))
     #:literals (quote cons)
     [pattern (bin id n ...)
              #:with test #'(λ (mch)
-                             (equal? id mch))
+                             (equal? id (second mch))) ;; bit equal?
              #:with (vars ...) #'(id)]
+
+
+    
+    [pattern x:id
+             #:with test #'(λ (z) (list z))
+             #:with (vars ...) #'(x)]
+    [pattern (quote v)
+             #:with test #'(λ (z) (and (equal? 'v z)
+                                       (list)))
+             #:with (vars ...) #'()]
     ))
 
 (define-syntax (bit-match stx)
@@ -28,14 +35,17 @@
            ...
            [else (error 'bit-match "no matching pattern for ~v" e)]))]))
 
-(bit-match '(bin z 10)
-  [(bin x n) (add1 n)])
 
-(bit-match (bin z 10)
-  [(bin x n) x])
-
-(bit-match (bin z 10)
-  [(bin x n) (begi x n)])
-
-
-
+(define z 10)
+;
+;
+;(bit-match '(bin z 10)
+;  [(bin x n) (add1 n)])
+;
+;
+;
+;(bit-match '(bin z 10)
+;  [(bin x n) x])
+;
+;(bit-match '(bin z 10)
+;  [(bin x n) (begi x n)])
