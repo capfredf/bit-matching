bbfa0e4cefc6a42fea220a7e73818c9117b1ff18
Joshua Larkin <joslarki@iu.edu>
2018-11-21 17:06:18 -0600

    match v2

--- match.rkt
+++ match.rkt
@@ -1,25 +1,18 @@
 #lang racket
 
-(require (for-syntax syntax/parse))
+(require rackunit
+         (for-syntax syntax/parse))
 
 (begin-for-syntax
   (define-syntax-class pat
     #:attributes ((vars 1) (test 0))
     #:literals (quote cons)
-    [pattern (bin id n ...)
+    [pattern (bin (id n) ...)
              #:with test #'(λ (mch)
-                             (equal? id (second mch))) ;; bit equal?
-             #:with (vars ...) #'(id)]
-
-
-    
-    [pattern x:id
-             #:with test #'(λ (z) (list z))
-             #:with (vars ...) #'(x)]
-    [pattern (quote v)
-             #:with test #'(λ (z) (and (equal? 'v z)
-                                       (list)))
-             #:with (vars ...) #'()]
+                             (and (= (length '(id ...))
+                                     (length (cdr mch)))
+                                  (map car (cdr mch)))) ;; bit equal?
+             #:with (vars ...) #'(id ...)]
     ))
 
 (define-syntax (bit-match stx)
@@ -36,16 +29,41 @@
            [else (error 'bit-match "no matching pattern for ~v" e)]))]))
 
 
-(define z 10)
+;(define-values (x y z) (10 10 10))
 ;
+;(bit-match (bin x 4 y 2 z 20)
+;           [(bin e1 n1 e2 n2 e3 n3) (+ e1 e2 e3)])
+
+(bit-match (bytes 40 10)
+  ((bin x 8 y 8) (+ x y))) ;; ==> 50
+
+(bit-match (bytes 16)
+ ((bin x 4 y 4) (+ x y))) ; ==> 1
+
+
+
+(bit-match '(bin (40 10) (200 60))
+  [(bin (y n)) y]
+  [(bin (y n) (z m)) (+ y z)])
+
+(check-equal? (bit-match '(bin (40 10))
+                         [(bin (y n)) y])
+              40)
+
+
+
 ;
-;(bit-match '(bin z 10)
+;(bit-match '(bin 40 10)
+;  [(bin x n) n])
+
+
+;(define z 10)
+;
+;(bit-match `(bin ,z 10)
 ;  [(bin x n) (add1 n)])
 ;
-;
-;
-;(bit-match '(bin z 10)
+;(bit-match `(bin ,z 10)
 ;  [(bin x n) x])
 ;
-;(bit-match '(bin z 10)
+;(bit-match `(bin ,z 10)
 ;  [(bin x n) (begi x n)])
